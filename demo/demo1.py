bdf0572bb3f862cb26b07dc4f0ed53427fd4d769
乐思宇 <lesiyu129@163.com>
2024-09-30 09:39:05 +0800

    结构化`demo1.py`代码，移动主逻辑到`Demo1`类中，增加`run`方法，并在`main.py`中调用。

--- demo/demo1.py
+++ demo/demo1.py
@@ -10,60 +10,66 @@ from tools.print_images_and_lables_4 import PrintImagesAndLables
 from models.LeNet import LeNet
 from tools.get_device_type import device
 
-if __name__ == '__main__':
-    transform = transforms.Compose([transforms.ToTensor()])
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    mean, std = calculate_mean_standardDeviation.GetMeanStd(
-        trainset).get_mean_std()
-    transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Normalize(mean, std)
-                                    ])
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            transform=transform)
-    trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=4, shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           transform=transform)
-    testloader = torch.utils.data.DataLoader(
-        testset, batch_size=4, shuffle=False, num_workers=2)
-    classes = ('plane', 'car', 'bird', 'cat',
-               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    # PrintImagesAndLables(trainloader, classes).show_images()
-    model = LeNet().to(device=device)
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    model.train()
-    for epoch in range(2):
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+class Demo1:
+    def __init__(self, num_epochs=5):
+        self.num_epochs = num_epochs
+        pass
 
-            running_loss += loss.item()
-            if i % 2000 == 1999:
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-    print('Finished Training')
+    def run(self):
+        transform = transforms.Compose([transforms.ToTensor()])
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                                download=True, transform=transform)
+        mean, std = calculate_mean_standardDeviation.GetMeanStd(
+            trainset).get_mean_std()
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize(mean, std)
+                                        ])
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                                transform=transform)
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=4, shuffle=True, num_workers=2)
 
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        model.eval()
-        for data in testloader:
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                               transform=transform)
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=4, shuffle=False, num_workers=2)
+        classes = ('plane', 'car', 'bird', 'cat',
+                   'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        # PrintImagesAndLables(trainloader, classes).show_images()
+        model = LeNet().to(device=device)
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        model.train()
+        for epoch in range(self.num_epochs):
+            running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
 
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
+                running_loss += loss.item()
+                if i % 2000 == 1999:
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, i + 1, running_loss / 2000))
+                    running_loss = 0.0
+        print('Finished Training')
+
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            model.eval()
+            for data in testloader:
+                images, labels = data
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print('Accuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct / total))
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 from tools.get_device_type import device
-print(device)
+import demo.demo1 as d
+if __name__ == '__main__':
+    print(device)
+    d.Demo1().run()
